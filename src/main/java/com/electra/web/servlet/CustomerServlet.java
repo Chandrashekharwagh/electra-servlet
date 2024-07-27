330bc8d141ba6a16fd1f28b80497eedbe5655aa1
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-07-28 04:27:14 +0530

    commits

--- src/main/java/com/electra/web/servlet/CustomerServlet.java
+++ src/main/java/com/electra/web/servlet/CustomerServlet.java
@@ -1,37 +1,21 @@
 package com.electra.web.servlet;
 
-import jakarta.servlet.annotation.WebServlet;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet("/submit_customer")public class CustomerServlet {
-    private String message;
+public class CustomerServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String addressId = request.getParameter("address_id");
 
-    public void init() {
-        System.out.println("--------------- inside the init() method ---------------");
-        message = "Hello Users!";
-    }
+        // Process the data (e.g., save to database)
+        // ...
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        System.out.println("--------------- inside the doGet() method ---------------");
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        System.out.println("--------------- inside the service() method ---------------");
-        this.doGet(request, response);
-    }
-
-    public void destroy() {
-        System.out.println("--------------- inside the destroy() method ---------------");
+        response.getWriter().println("Customer submitted successfully!");
     }
 }
